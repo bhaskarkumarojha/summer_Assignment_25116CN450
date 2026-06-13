d222af6ac467ca3efb4fda6b2d99920fffccf03f
bhaskarkumarojha <bhaskarkumarojha4@gmail.com>
2026-06-13 13:26:02 +0530

    day_6

---
+++ Day_6/withoutpower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, n, i;
+    long long result = 1;
+
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &x, &n);
+
+    for (i = 0; i < n; i++) {
+        result = result * x;
+    }
+
+    printf("%d^%d = %lld\n", x, n, result);
+
+    return 0;
+}
