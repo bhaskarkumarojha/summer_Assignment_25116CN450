56fc1c55ed758d8bb02975801004d0520ef5eb47
bhaskarkumarojha <bhaskarkumarojha4@gmail.com>
2026-06-13 13:34:42 +0530

    day_7

---
+++ Day_7/reverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int reverseNumber(int n, int rev) {
+    if (n == 0)
+        return rev;
+
+    return reverseNumber(n / 10, rev * 10 + n % 10);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Reversed number = %d\n", reverseNumber(n, 0));
+
+    return 0;
+}
