acef883a06f985d7822c39286057eac45c8d14c5
bhaskarkumarojha <bhaskarkumarojha4@gmail.com>
2026-06-15 20:38:17 +0530

    day_15

---
+++ Day_15/rotatearrayleft.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, d;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter number of right rotations: ");
+    scanf("%d", &d);
+
+    d = d % n;
+
+    while(d--)
+    {
+        int last = arr[n - 1];
+
+        for(i = n - 1; i > 0; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+    }
+
+    printf("Array after right rotation:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
